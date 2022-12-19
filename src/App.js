ef0c1b31e43343c2062335ffa228200e243d0dfe
Sharon Kasper <sharon@sharon-kasper.com>
2022-12-18 18:25:05 -0800

    start of testing

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
 
   return (
     <div className="text-center">
-      <h1 className="text-4xl my-10 font-bold">Online Dictionary!</h1>
+      <h1 className="text-4xl my-10 font-bold" data-testid='header-element'>Online Dictionary!</h1>
       <form className="flex justify-center mb-8">
         <input type="text" name="word" id="word" placeholder="Look it up" ref={inputRef} className="border-b-4 border-white bg-black mr-8 pl-4"  />
         <NeoBrutalistButton type='submit' id="submit" onClick={handleClick} label="Look it up" />
